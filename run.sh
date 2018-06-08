bc855d1409be240546ef97ac561213320b279d31
Ashok Devatwal <ashokdev78@gmail.com>
2018-06-08 13:43:11 +0530

    Signle DB with Gdrive Backup

--- run.sh
+++ run.sh
@@ -3,7 +3,17 @@
 # Load Settings file 
 . .settings
 
-# local backup path
+if [ "$backupMode" == "gdrive" ]; then		
+   #Check Gdrive backup Root folder exists on gdrive
+   gdriveRSID=$(gdrive list --no-header | grep $gdriveDir | grep dir | awk '{ print $1}')
+   if [ -z "$gdriveRSID" ]; then
+      gdrive mkdir $gdriveDir
+      gdriveRSID=$(gdrive list --no-header | grep $gdriveDir | grep dir | awk '{ print $1}')
+   fi
+fi
+
+
+# Local Backup Root Dir
 mkdir -p $localPath
 
 if [ "$multiDB" == "true" ]; then
@@ -15,12 +25,29 @@ if [ "$multiDB" == "true" ]; then
         # Multi DBS with Multi users    
 	echo "multi db multi user"
     fi
-else
-    # Single database
-    echo "Mode : Single Database"
+else   
+    # Local Backup Dir
+    mkdir -p $localPath$db1
+    
+    # Local Backup file
+    backupFile=$localPath$db1"/"$db1.sql
+	
     #Back up the Mysql Database
-    mysqldump --user=$mysqlUser --password=$mysqlPassword $db1 > $localPath$db1.sql
+    mysqldump --user=$mysqlUser --password=$mysqlPassword $db1 > $backupFile
     echo "Local Backup Created"
-    echo "Path : $localPath$db1.sql"
+    echo "Path : $backupFile"
+
+    if [ "$backupMode" == "gdrive" ]; then
+	#Check Gdrive backup folder exists on gdrive
+        gdriveSID=$(gdrive list --no-header | grep $db1 | grep dir | awk '{ print $1}')
+        if [ -z "$gdriveSID" ]; then
+         gdrive mkdir --parent $gdriveRSID $db1
+         gdriveSID=$(gdrive list --no-header | grep $db1 | grep dir | awk '{ print $1}')
+        fi
+    fi
+
+    #Upload Mysql database
+    gdrive upload --parent $gdriveSID $backupFile	
+
 fi
 
